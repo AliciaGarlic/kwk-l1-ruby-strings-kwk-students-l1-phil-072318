7a0f9db2d50b585a8861e708418fec17c0e187f9
Alicia Garlic <purplia23@gmail.com>
2018-07-23 19:32:48 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -6,10 +6,10 @@ party_name = gets.chomp
 date = gets.chomp
 time = gets.chomp
 host_name = gets.chomp
-puts "Dear guest_name,
+puts "Dear #{guest_name},
  
-You are cordially invited to the party_name on date at time. Please RSVP no later than October 30.
+You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30.
  
 Sincerely,
  
-host_name"
+#{host_name}"
